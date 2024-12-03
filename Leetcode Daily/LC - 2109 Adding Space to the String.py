f131e47ec92a17204e8cfe8de81e855d54bea0ec
Yash Mahajan <58283608+yashmahaja@users.noreply.github.com>
2024-12-03 02:50:24 -0500

    LC - 2109 Adding Space to the String

--- Leetcode Daily/LC - 2109 Adding Space to the String.py
+++ Leetcode Daily/LC - 2109 Adding Space to the String.py
@@ -10,17 +10,30 @@ Return the modified string after the spaces have been added.
 
 """
 def addSpaces(s, spaces):
-    ans = []
-    pointer = 0
-    for space in spaces:
-            ans.append(s[pointer:space])
-            pointer = space
-    ans.append(s[pointer:])
-    ans = " ".join(ans)
-    return ans
+    # ans = []
+    # pointer = 0
+    # for space in spaces:
+    #         ans.append(s[pointer:space])
+    #         pointer = space
+    # ans.append(s[pointer:])
+    # ans = " ".join(ans)
+    # return ans
 
+    i, j = 0, 0
+    res = []
 
+    while i < len(s) and j < len(spaces):
+        if i < spaces[j]:
+            res.append(s[i])
+            i += 1
+        else:
+            res.append(" ")
+            j += 1
 
-s = "spacing"
-spaces = [0,1,2,3,4,5,6]
+    if i < len(s):
+        res.append(s[i:])
+    return "".join(res)
+
+s = "LeetcodeHelpsMeLearn"
+spaces = [8,13,15]
 print(addSpaces(s,spaces))
